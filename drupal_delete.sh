41dea5b3dd500ee18ede3f12108817e02609529d
Galata Tona <gtona@ginkgostreet.com>
2015-01-27 12:52:22 -0500

    Modified mysql command and confirmed it works

--- drupal_delete.sh
+++ drupal_delete.sh
@@ -8,13 +8,10 @@ CALLPATH=`dirname "$0"`
 ABS_CALLPATH="`( cd \"${CALLPATH}\" && pwd -P)`"
 
 # Read Configurations
-. ${ABS_CALLPATH}/utils/migrate_wp2drupal.conf
+. ${ABS_CALLPATH}/migrate_wp2drupal.conf
 
 echo "Deleting Drupal Site..."
-sudo rm -r ${PROJ_ROOT}/${PROJ_NAME}/${WEB_DIR}
+sudo rm -r ${PROJ_ROOT}/${WEB_DIR}
 
 echo "Dropping databases..."
-mysql
-DROP DATABASE membership_drupal
-DROP DATABASE membership_civi
-
+mysql < delete_drupal.sql
